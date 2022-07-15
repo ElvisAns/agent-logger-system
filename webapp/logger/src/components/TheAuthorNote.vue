ab3562a58d14de6b109cdec6ecb1ac26d806c5ed
ElvisAns <ansimapersic@gmail.com>
2022-07-15 03:27:15 +0200

    [NEW] created the authorNote Component

---
+++ webapp/logger/src/components/TheAuthorNote.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="navbar-footer">
+        <div class="container">
+          <h2>Software Packed by</h2> <span>Elvis Ansima</span> <a href="https://github.com/ElvisAns" target="_blank" >@ElvisAns</a> </div>
+      </div>
+</template>
+
+<script>
+    export default {
+        name: TheAuthorNote
+    }
+</script>
